e746511c75b7a01a7d5350c7a06627befb341df8
Ary Davani <arydavani@gmail.com>
2017-06-28 14:26:54 -0400

    made add todo actually work

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const models = require('./models');
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 
 const app = express();
@@ -9,11 +11,29 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.get('/', function(req, res) {
 
   models.Todo.findAll().then(function(todos) {
-    res.render('index', {todos});
+    res.render('index', {todos: todos});
   });
 });
+
+app.post('/', function(req, res) {
+  let newItem = req.body.todo;
+
+  models.Todo.create({
+    item: newItem
+  });
+
+  models.Todo.findAll().then(function(todos) {
+    res.render('index', {todos: todos});
+  });
+});
+
+
 
 app.listen(3000);
