78b6e0e4647c04849a3c0392a421d342e399e217
Ary Davani <arydavani@gmail.com>
2017-07-10 16:13:58 -0400

    added editing feature

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 
 app.engine('mustache', mustacheExpress());
+
 app.set('view engine', 'mustache');
 app.set('views', './views');
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -47,8 +50,20 @@ app.post('/complete', function(req, res) {
   });
 });
 
-app.post('/clear', function(req, res) {
+app.post('/edit', function(req, res) {
+  let item = req.body.item;
+  let assignedto = req.body.assignedto;
+  let idEdit = req.body.id;
 
-})
+  models.Todo.update(
+    {
+      item: item,
+      assignedto: assignedto
+    },
+    {where: {id: idEdit}}
+  ).then(function() {
+    res.redirect('/');
+  });
+});
 
 app.listen(3000);
